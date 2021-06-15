6edff295e0110823d9bf42fc87af724027db7329
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-06-16 07:02:29 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,19 +37,18 @@
     </section>
 
     <section>
-      
+      <div id="contact-wrapper">
+
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
